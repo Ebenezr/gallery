bdca75fbfabbd94bfa88c66a8899f5eb2035b3e2
john mutavi <jonnygovish@gmail.com>
2020-08-06 23:18:42 +0300

    add config

--- server.js
+++ server.js
@@ -4,24 +4,36 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const config = require('./_config');
+const config = require('./_config');
 
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
 
-mongoose.connect(
-  config.mongoURI.development,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err) => {
-    if (err) console.log(err);
+// Initializing the app
+const app = express();
+
+// connecting the database
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || config.mongoURI[app.settings.env];
+mongoose.connect(MONGODB_URI, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Connected to Database: ${MONGODB_URI}`);
   }
-);
+});
 
 // test if the database has connected successfully
 let db = mongoose.connection;
 db.once('open', () => {
   console.log('Database connected successfully');
 });
+// let db = mongoose.connection;
+// db.once('open', ()=>{
+//     console.log('Database connected successfully')
+// })
 
 // Initializing the app
 const app = express();
@@ -42,3 +54,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is listening at http://localhost:${PORT}`);
 });
+
+module.exports = app;
