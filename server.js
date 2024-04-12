be0b90e4d1ab0945a597dd0cacadfb33b1c5f682
ebukosia <ebukosia@safaricom.co.ke>
2024-04-12 13:15:55 +0300

    feat: connect to mongo atlas db

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const config = require('./_config');
 
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
 
-// connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
-let dbName = 'darkroom';
-mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
-    if (err) console.log(err)
-});
+mongoose.connect(
+  config.mongoURI.development,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) console.log(err);
+  }
+);
 
 // test if the database has connected successfully
 let db = mongoose.connection;
-db.once('open', ()=>{
-    console.log('Database connected successfully')
-})
+db.once('open', () => {
+  console.log('Database connected successfully');
+});
 
 // Initializing the app
 const app = express();
-
 
 // View Engine
 app.set('view engine', 'ejs');
@@ -31,16 +33,12 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // body parser middleware
-app.use(express.json())
-
+app.use(express.json());
 
 app.use('/', index);
 app.use('/image', image);
 
-
-
- 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server is listening at http://localhost:${PORT}`);
 });
